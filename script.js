bedb852ffa7b5061945bc445fae845f6ed9f5323
akashks <akashks8855@gmail.com>
2024-05-30 01:36:26 +0530

    added scroll animation using scroll reveal

---
+++ script.js
@@ -0,0 +1,27 @@
+ScrollReveal().reveal('.home',{ easing: 'ease-in' , distance: '-150px' ,duration: 1200, opacity: 0 });
+
+ScrollReveal().reveal('.about_image',{ easing: 'ease-in' , origin: 'right',distance: '-170px' ,duration: 1600, opacity: 0 });
+
+ScrollReveal().reveal('.about_data',{ easing: 'ease-in' , origin: 'left',distance: '-170px' ,duration: 1600, opacity: 0 });
+
+ScrollReveal().reveal('.functions_title',{ easing: 'ease-in' , origin: 'bottom',distance: '-100px' ,duration: 1600, opacity: 0 });
+
+ScrollReveal().reveal('.functions_subtitle',{ easing: 'ease-in' , origin: 'bottom',distance: '-100px' ,duration: 1600, opacity: 0 });
+
+ScrollReveal().reveal('.functions_list-container',{ easing: 'ease-in' , origin: 'bottom',distance: '-100px' ,duration: 1600, opacity: 0 });
+
+ScrollReveal().reveal('.products_subtitle',{ easing: 'ease-in' , origin: 'bottom',distance: '-100px' ,duration: 1600, opacity: 0 });
+
+ScrollReveal().reveal('.products_title',{ easing: 'ease-in' , origin: 'bottom',distance: '-100px' ,duration: 1600, opacity: 0 });
+
+ScrollReveal().reveal('.products_list-container',{ easing: 'ease-in' , origin: 'bottom',distance: '-100px' ,duration: 1600, opacity: 0 });
+
+ScrollReveal().reveal('.faqs_subtitle',{ easing: 'ease-in' , origin: 'bottom',distance: '-100px' ,duration: 1600, opacity: 0 });
+
+ScrollReveal().reveal('.faqs_title',{ easing: 'ease-in' , origin: 'bottom',distance: '-100px' ,duration: 1600, opacity: 0 });
+
+ScrollReveal().reveal('.faqs_subtitle-secondary',{ easing: 'ease-in' , origin: 'bottom',distance: '-100px' ,duration: 1600, opacity: 0 });
+
+ScrollReveal().reveal('.faqs_container',{ easing: 'ease-in' , origin: 'bottom',distance: '-100px' ,duration: 1600, opacity: 0 });
+
+ScrollReveal().reveal('.footer',{ easing: 'ease-in' , origin: 'bottom',distance: '-100px' ,duration: 1600, opacity: 0 });
