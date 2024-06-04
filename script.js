2e8f7e348be112f18bf3ac7380d34cdea3d5395d
akashks <akashks8855@gmail.com>
2024-06-04 12:38:17 +0530

    made responsive

--- script.js
+++ script.js
@@ -1,27 +1,28 @@
-ScrollReveal().reveal('.home',{ easing: 'ease-in' , distance: '-150px' ,duration: 1200, opacity: 0 });
+ScrollReveal().reveal('.home',{ easing: 'ease-in' , distance: '60px' ,duration: 2500, opacity: 0, delay:400, origin:'top' });
 
-ScrollReveal().reveal('.about_image',{ easing: 'ease-in' , origin: 'right',distance: '-170px' ,duration: 1600, opacity: 0 });
+ScrollReveal().reveal('.about_image',{ easing: 'ease-in' , origin: 'left',distance: '60px' ,duration: 2500, opacity: 0,delay:400 });
 
-ScrollReveal().reveal('.about_data',{ easing: 'ease-in' , origin: 'left',distance: '-170px' ,duration: 1600, opacity: 0 });
+ScrollReveal().reveal('.about_data',{ easing: 'ease-in' , origin: 'right',distance: '60px' ,duration: 2500, opacity: 0,delay:400 });
 
-ScrollReveal().reveal('.functions_title',{ easing: 'ease-in' , origin: 'bottom',distance: '-100px' ,duration: 1600, opacity: 0 });
+ScrollReveal().reveal('.functions_title',{ easing: 'ease-in' , origin: 'top',distance: '-60px' ,duration: 2500, opacity: 0,delay:400 });
 
-ScrollReveal().reveal('.functions_subtitle',{ easing: 'ease-in' , origin: 'bottom',distance: '-100px' ,duration: 1600, opacity: 0 });
+ScrollReveal().reveal('.functions_subtitle',{ easing: 'ease-in' , origin: 'top',distance: '60px' ,duration: 2500, opacity: 0,delay:400 });
 
-ScrollReveal().reveal('.functions_list-container',{ easing: 'ease-in' , origin: 'bottom',distance: '-100px' ,duration: 1600, opacity: 0 });
+ScrollReveal().reveal('.functions_list-container',{ easing: 'ease-in' , origin: 'top',distance: '60px' ,duration: 2500, opacity: 0,delay:400 });
 
-ScrollReveal().reveal('.products_subtitle',{ easing: 'ease-in' , origin: 'bottom',distance: '-100px' ,duration: 1600, opacity: 0 });
 
-ScrollReveal().reveal('.products_title',{ easing: 'ease-in' , origin: 'bottom',distance: '-100px' ,duration: 1600, opacity: 0 });
+ScrollReveal().reveal(`.products_subtitle, .products_title, .products_list-container`,{ easing: 'ease-in' , origin: 'top',distance: '60px' ,duration: 2500, opacity: 0,delay:400 });
 
-ScrollReveal().reveal('.products_list-container',{ easing: 'ease-in' , origin: 'bottom',distance: '-100px' ,duration: 1600, opacity: 0 });
+// ScrollReveal().reveal('.products_title',{ easing: 'ease-in' , origin: 'top',distance: '60px' ,duration: 2500, opacity: 0,delay:400 });
 
-ScrollReveal().reveal('.faqs_subtitle',{ easing: 'ease-in' , origin: 'bottom',distance: '-100px' ,duration: 1600, opacity: 0 });
+// ScrollReveal().reveal('.products_list-container',{ easing: 'ease-in' , origin: 'top',distance: '60px' ,duration: 2500, opacity: 0,delay:400 });
 
-ScrollReveal().reveal('.faqs_title',{ easing: 'ease-in' , origin: 'bottom',distance: '-100px' ,duration: 1600, opacity: 0 });
+ScrollReveal().reveal('.faqs_subtitle',{ easing: 'ease-in' , origin: 'top',distance: '60px' ,duration: 2500, opacity: 0,delay:400,mobile:false });
 
-ScrollReveal().reveal('.faqs_subtitle-secondary',{ easing: 'ease-in' , origin: 'bottom',distance: '-100px' ,duration: 1600, opacity: 0 });
+ScrollReveal().reveal('.faqs_title',{ easing: 'ease-in' , origin: 'top',distance: '60px' ,duration: 2500, opacity: 0,delay:400,mobile:false });
 
-ScrollReveal().reveal('.faqs_container',{ easing: 'ease-in' , origin: 'bottom',distance: '-100px' ,duration: 1600, opacity: 0 });
+ScrollReveal().reveal('.faqs_subtitle-secondary',{ easing: 'ease-in' , origin: 'top',distance: '60px' ,duration: 2500, opacity: 0,delay:400,mobile:false });
 
-ScrollReveal().reveal('.footer',{ easing: 'ease-in' , origin: 'bottom',distance: '-100px' ,duration: 1600, opacity: 0 });
+ScrollReveal().reveal('.faqs_container',{ easing: 'ease-in' , origin: 'top',distance: '60px' ,duration: 2500, opacity: 0,delay:400,mobile:false });
+
+ScrollReveal().reveal('.footer',{ easing: 'ease-in' , origin: 'top',distance: '60px' ,duration: 2500, opacity: 0,delay:400, mobile:false });
